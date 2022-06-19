d8831b77fe8dc33fd5fb0c0130e7b42b37cc1a41
Fernando Silva <fernandosilvadj@hotmail.com>
2022-06-18 21:06:40 -0300

    feat: add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kFooterHeight = 50.0;
+
+const Color kActiveCardColour = Color(0xFF1D1E33);
+const Color kInactiveCardColour = Color(0xFF111328);
+const Color kFooterColour = Color(0xFFEB1555);
+
+const TextStyle kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
